56a1e07cf530b1984371f7101a53405e991b16e5
Mike Alfare <mike.alfare@dbtlabs.com>
2024-05-02 15:09:39 -0400

    revert changes from fork

--- dbt/include/synapse/macros/adapters/show.sql
+++ dbt/include/synapse/macros/adapters/show.sql
@@ -1,3 +1,13 @@
+{% macro get_show_sql(compiled_code, sql_header, limit) -%}
+  {%- if sql_header -%}
+  {{ sql_header }}
+  {%- endif -%}
+  {%- if limit is not none -%}
+  {{ get_limit_subquery_sql(compiled_code, limit) }}
+  {%- else -%}
+  {{ compiled_code }}
+  {%- endif -%}
+{% endmacro %}
 
 {% macro get_limit_subquery_sql(sql, limit) %}
   {{ adapter.dispatch('get_limit_subquery_sql', 'dbt')(sql, limit) }}
